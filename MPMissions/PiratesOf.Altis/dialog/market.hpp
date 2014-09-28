25bfd96d38d76ae5623b731f6de45be0dc25028e
Flo122 <florian.kokott@googlemail.com>
2014-09-28 15:34:48 +0200

    Added Market Dialog

--- MPMissions/PiratesOf.Altis/dialog/market.hpp
+++ MPMissions/PiratesOf.Altis/dialog/market.hpp
@@ -1,99 +1,163 @@
+//Grid macros
+#define GUI_GRID_X	(0)
+#define GUI_GRID_Y	(0)
+#define GUI_GRID_W	(0.025)
+#define GUI_GRID_H	(0.04)
+#define GUI_GRID_WAbs	(1)
+#define GUI_GRID_HAbs	(1)
+
 class marketView
 {
 	idd = 39500;
-	name = "marketView";
-	movingEnable = false;
+	movingEnabled = false;
 	enableSimulation = true;
 	
 	class controlsBackground
 	{
-		class RscTitleBackground : Life_RscText
+		class RscStructuredText_1100: RscStructuredText
 		{
-			colorBackground[] = {"(profilenamespace getvariable ['GUI_BCG_RGB_R',0.3843])", "(profilenamespace getvariable ['GUI_BCG_RGB_G',0.7019])", "(profilenamespace getvariable ['GUI_BCG_RGB_B',0.8862])", "(profilenamespace getvariable ['GUI_BCG_RGB_A',0.7])"};
-			idc = -1;
-			x = 0.1;
-			y = 0.2;
-			w = 0.7;
-			h = (1 / 25);
+			idc = 1100;
+			text = "<t size='1.2' align='center'>Altis Markt</t>"; 
+			x = 5 * GUI_GRID_W + GUI_GRID_X;
+			y = 1 * GUI_GRID_H + GUI_GRID_Y;
+			w = 30 * GUI_GRID_W;
+			h = 1 * GUI_GRID_H;
+			colorBackground[] = {0,0,0,0.8};
 		};
-		
-		class RscBackground : Life_RscText
+		class IGUIBack_2200: IGUIBack
 		{
-			colorBackground[] = {0, 0, 0, 0.7};
-			idc = -1;
-			x = 0.1;
-			y = 0.2 + (11 / 250);
-			w = 0.7;
-			h = 0.7 - (22 / 250);
+			idc = 2200;
+			x = 5 * GUI_GRID_W + GUI_GRID_X;
+			y = 2 * GUI_GRID_H + GUI_GRID_Y;
+			w = 30 * GUI_GRID_W;
+			h = 21 * GUI_GRID_H;
 		};
-		
-		class RscTitleText : Life_RscTitle
+		class lblResourceName: RscText
 		{
-			colorBackground[] = {0, 0, 0, 0};
-			idc = 39001;
-			text = "";
-			x = 0.1;
-			y = 0.2;
-			w = 0.7;
-			h = (1 / 25);
+			idc = 1000;
+			text = "Resource"; 
+			x = 16.5 * GUI_GRID_W + GUI_GRID_X;
+			y = 2.5 * GUI_GRID_H + GUI_GRID_Y;
+			w = 18 * GUI_GRID_W;
+			h = 2.5 * GUI_GRID_H;
+			sizeEx = 2 * GUI_GRID_H;
 		};
-		
-		
-		class RscTrunkText : Life_RscText
+		class IGUIBack_2201: IGUIBack
 		{
-			idc = -1;
-			colorBackground[] = {"(profilenamespace getvariable ['GUI_BCG_RGB_R',0.3843])", "(profilenamespace getvariable ['GUI_BCG_RGB_G',0.7019])", "(profilenamespace getvariable ['GUI_BCG_RGB_B',0.8862])", 0.5};
-			text = "Einkaufspreis";
-			sizeEx = 0.04;
-			
-			x = 0.11; y = 0.25;
-			w = 0.3; h = 0.04;
+			idc = 2201;
+			x = 17 * GUI_GRID_W + GUI_GRID_X;
+			y = 5.5 * GUI_GRID_H + GUI_GRID_Y;
+			w = 17 * GUI_GRID_W;
+			h = 2.5 * GUI_GRID_H;
 		};
-		
-		class RscPlayerText : Life_RscText
+		class lblResourcePrice: RscText
 		{
-			idc = -1;
-			colorBackground[] = {"(profilenamespace getvariable ['GUI_BCG_RGB_R',0.3843])", "(profilenamespace getvariable ['GUI_BCG_RGB_G',0.7019])", "(profilenamespace getvariable ['GUI_BCG_RGB_B',0.8862])", 0.5};
-			text = "Verkaufspreis";
-			sizeEx = 0.04;
-			
-			x = 0.49; y = 0.25;
-			w = 0.3; h = 0.04;
-		};	
+			idc = 1001;
+			text = "XXXXXXXXXXXXX$"; 
+			x = 17 * GUI_GRID_W + GUI_GRID_X;
+			y = 5.5 * GUI_GRID_H + GUI_GRID_Y;
+			w = 17 * GUI_GRID_W;
+			h = 2.5 * GUI_GRID_H;
+			colorText[] = {0,1,0,1};
+			sizeEx = 1.3 * GUI_GRID_H;
+		};
+		class IGUIBack_2202: IGUIBack
+		{
+			idc = 2202;
+			x = 17 * GUI_GRID_W + GUI_GRID_X;
+			y = 10 * GUI_GRID_H + GUI_GRID_Y;
+			w = 17 * GUI_GRID_W;
+			h = 12 * GUI_GRID_H;
+		};
+		class RscStructuredText_1101: RscStructuredText
+		{
+			idc = 1101;
+			text = "<t align='center'>Entwicklung</t>"; 
+			x = 17 * GUI_GRID_W + GUI_GRID_X;
+			y = 9 * GUI_GRID_H + GUI_GRID_Y;
+			w = 17 * GUI_GRID_W;
+			h = 1 * GUI_GRID_H;
+			colorBackground[] = {0,0,0,0.8};
+		};
+		class picTrendGlobal: RscPicture
+		{
+			idc = 1200;
+			text = "#(argb,8,8,3)color(1,1,1,1)";
+			x = 17.5 * GUI_GRID_W + GUI_GRID_X;
+			y = 10.5 * GUI_GRID_H + GUI_GRID_Y;
+			w = 3 * GUI_GRID_W;
+			h = 2.5 * GUI_GRID_H;
+		};
+		class picTrendLocal: RscPicture
+		{
+			idc = 1201;
+			text = "#(argb,8,8,3)color(1,1,1,1)";
+			x = 17.5 * GUI_GRID_W + GUI_GRID_X;
+			y = 13.5 * GUI_GRID_H + GUI_GRID_Y;
+			w = 3 * GUI_GRID_W;
+			h = 2.5 * GUI_GRID_H;
+		};
+		class RscText_1002: RscText
+		{
+			idc = 1002;
+			text = "Seit Serverstart"; 
+			x = 20.5 * GUI_GRID_W + GUI_GRID_X;
+			y = 10.5 * GUI_GRID_H + GUI_GRID_Y;
+			w = 13 * GUI_GRID_W;
+			h = 1 * GUI_GRID_H;
+			sizeEx = 1.2 * GUI_GRID_H;
+		};
+		class RscText_1003: RscText
+		{
+			idc = 1003;
+			text = "Vor kurzem"; 
+			x = 20.5 * GUI_GRID_W + GUI_GRID_X;
+			y = 13.5 * GUI_GRID_H + GUI_GRID_Y;
+			w = 13 * GUI_GRID_W;
+			h = 1 * GUI_GRID_H;
+			sizeEx = 1.2 * GUI_GRID_H;
+		};
+		class lblTrendGlobal: RscText
+		{
+			idc = 1004;
+			text = "xxxxxxxx$"; 
+			x = 20.5 * GUI_GRID_W + GUI_GRID_X;
+			y = 11.5 * GUI_GRID_H + GUI_GRID_Y;
+			w = 13 * GUI_GRID_W;
+			h = 1 * GUI_GRID_H;
+		};
+		class lblTrendLocal: RscText
+		{
+			idc = 1005;
+			text = "xxxxxxxx$"; 
+			x = 20.5 * GUI_GRID_W + GUI_GRID_X;
+			y = 14.5 * GUI_GRID_H + GUI_GRID_Y;
+			w = 13 * GUI_GRID_W;
+			h = 1 * GUI_GRID_H;
+		};
 	};
 	
-	class Controls
+	class controls
 	{
-		class achatGear : Life_RscListBox
+		class bClose: RscButtonMenu
 		{
-			idc = 39002;
-			text = "";
-			sizeEx = 0.030;
-			
-			x = 0.11; y = 0.29;
-			w = 0.3; h = 0.55;
+			idc = 2400;
+			text = "Schliessen"; 
+			x = 5 * GUI_GRID_W + GUI_GRID_X;
+			y = 23 * GUI_GRID_H + GUI_GRID_Y;
+			w = 30 * GUI_GRID_W;
+			h = 1 * GUI_GRID_H;
+			onButtonClick="closeDialog 0;";
 		};
-		
-		class VenteGear : Life_RscListBox
+		class listResources: RscListbox
 		{
-			idc = 39003;
-			text = "";
-			sizeEx = 0.030;
-			
-			x = 0.49; y = 0.29;
-			w = 0.3; h = 0.55;
-		};
-		
-		
-		class ButtonClose : Life_RscButtonMenu {
-			idc = -1;
-			//shortcuts[] = {0x00050000 + 2};
-			text = "$STR_Global_Close";
-			onButtonClick = "closeDialog 0;";
-			x = 0.1;
-			y = 0.9 - (1 / 25);
-			w = (6.25 / 40);
-			h = (1 / 25);
+			idc = 1500;
+			x = 5.5 * GUI_GRID_W + GUI_GRID_X;
+			y = 2.5 * GUI_GRID_H + GUI_GRID_Y;
+			w = 10.5 * GUI_GRID_W;
+			h = 19.5 * GUI_GRID_H;
+			onLBSelChanged = "[] spawn life_fnc_refreshMarketView";
 		};
 	};
 };
+
